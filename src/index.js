75675c3e83c3961d79d136647d4e8a01e1e4bab5
Koichi Miyamoto <m5813k@gmail.com>
2018-06-02 18:22:29 +0900

    split file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import matIV from "./minMatrix";
 import { create_vbo, create_program, create_shader } from "./util";
+import getMvpMatrix from "./mvpMatrix";
 
 window.onload = function() {
   // canvasエレメントを取得
   var c = document.getElementById("canvas");
-  c.width = 300;
-  c.height = 300;
+  c.width = 500;
+  c.height = 500;
 
-  // webglコンテキストを取得
-  var gl = c.getContext("webgl") || c.getContext("experimental-webgl");
+  var gl = c.getContext("webgl");
   window.gl = gl;
 
   //init canvas
@@ -23,9 +22,6 @@ window.onload = function() {
   // プログラムオブジェクトの生成とリンク
   var prg = create_program(v_shader, f_shader);
 
-  // attributeLocationの取得
-  var attLocation = gl.getAttribLocation(prg, "position");
-
   // attributeの要素数(この場合は xyz の3要素)
   var attStride = 3;
 
@@ -37,45 +33,26 @@ window.onload = function() {
     -1.0, 0.0, 0.0
   ];
 
-  // VBOの生成
+  //vbo
+  // VBOの生成とbind
   var vbo = create_vbo(vertex_position);
-
-  // VBOをバインド
   gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
 
-  // attribute属性を有効にする
+  // attributeLocationの取得
+  var attLocation = gl.getAttribLocation(prg, "position");
+  // attribute属性を有効 & 登録
   gl.enableVertexAttribArray(attLocation);
-
-  // attribute属性を登録
   gl.vertexAttribPointer(attLocation, attStride, gl.FLOAT, false, 0, 0);
 
-  // minMatrix.js を用いた行列関連処理
-  // matIVオブジェクトを生成
-  var m = new matIV();
-
-  // 各種行列の生成と初期化
-  var mMatrix = m.identity(m.create());
-  var vMatrix = m.identity(m.create());
-  var pMatrix = m.identity(m.create());
-  var mvpMatrix = m.identity(m.create());
-
-  // ビュー座標変換行列
-  m.lookAt([0.0, 1.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
-
-  // プロジェクション座標変換行列
-  m.perspective(90, c.width / c.height, 0.1, 100, pMatrix);
-
-  // 各行列を掛け合わせ座標変換行列を完成させる
-  m.multiply(pMatrix, vMatrix, mvpMatrix);
-  m.multiply(mvpMatrix, mMatrix, mvpMatrix);
+  //uniform
   // uniformLocationの取得
-  var uniLocation = gl.getUniformLocation(prg, "mvpMatrix");
-
   // uniformLocationへ座標変換行列を登録
+  var uniLocation = gl.getUniformLocation(prg, "mvpMatrix");
+  let mvpMatrix = getMvpMatrix(c.width, c.height);
   gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+
   // モデルの描画
   gl.drawArrays(gl.TRIANGLES, 0, 3);
-
   // コンテキストの再描画
   gl.flush();
 };
---
+++ src/mvpMatrix.js
@@ -0,0 +1,21 @@
+import matIV from "./minMatrix";
+var m = new matIV();
+export default (width, height) => {
+  // 各種行列の生成と初期化
+  var mMatrix = m.identity(m.create());
+  var vMatrix = m.identity(m.create());
+  var pMatrix = m.identity(m.create());
+  var mvpMatrix = m.identity(m.create());
+
+  // ビュー座標変換行列
+  m.lookAt([0.0, 1.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
+
+  // プロジェクション座標変換行列
+  m.perspective(90, width / height, 0.1, 100, pMatrix);
+
+  // 各行列を掛け合わせ座標変換行列を完成させる
+  m.multiply(pMatrix, vMatrix, mvpMatrix);
+  m.multiply(mvpMatrix, mMatrix, mvpMatrix);
+
+  return mvpMatrix;
+};
