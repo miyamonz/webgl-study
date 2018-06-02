c9c8b2cd6d23f0e3e7287f274f53a1e2964244dc
Koichi Miyamoto <m5813k@gmail.com>
2018-06-02 19:20:45 +0900

    model行列を変更して複数回描画

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 import { create_program, create_shader } from "./util";
 import registerVBO from "./registerVBO";
-import getMvpMatrix from "./mvpMatrix";
+import { multiply, getModel, getPV } from "./mvpMatrix";
 
-window.onload = function() {
-  // canvasエレメントを取得
-  var c = document.getElementById("canvas");
-  c.width = 500;
-  c.height = 500;
-
-  var gl = c.getContext("webgl");
-  window.gl = gl;
-
-  //init canvas
+let initCanvas = gl => {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clearDepth(1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+};
 
-  // 頂点シェーダとフラグメントシェーダの生成
-  var v_shader = create_shader("vs");
-  var f_shader = create_shader("fs");
-
-  // プログラムオブジェクトの生成とリンク
-  var prg = create_program(v_shader, f_shader);
-
+let prependVBO = prg => {
   // prettier-ignore
   var positions = [
     0.0, 1.0, 0.0, 
@@ -38,14 +24,45 @@ window.onload = function() {
     0.0, 0.0, 1.0, 1.0,
   ];
   registerVBO(prg, colors, 4, "color");
+};
+window.onload = function() {
+  // canvasエレメントを取得
+  var c = document.getElementById("canvas");
+  c.width = 500;
+  c.height = 500;
+  let size = {
+    width: c.width,
+    height: c.height
+  };
 
-  //uniform
+  var gl = c.getContext("webgl");
+  window.gl = gl;
+
+  initCanvas(gl);
+
+  var v_shader = create_shader("vs");
+  var f_shader = create_shader("fs");
+  var prg = create_program(v_shader, f_shader);
+
+  prependVBO(prg);
+
+  //prettier-ignore
+  let vecs = [
+    [0, 0, 0], 
+    [0, 1, 0],
+    [1, 1, 0],
+    [1, -2, -2]
+  ];
+
+  let pv = getPV(size);
   var uniLocation = gl.getUniformLocation(prg, "mvpMatrix");
-  let mvpMatrix = getMvpMatrix(c.width, c.height);
-  gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
-
-  // モデルの描画
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
-  // コンテキストの再描画
+  let drawWithMat = mat => {
+    gl.uniformMatrix4fv(uniLocation, false, mat);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+  };
+  vecs
+    .map(getModel)
+    .map(model => multiply(pv, model))
+    .forEach(drawWithMat);
   gl.flush();
 };
--- src/mvpMatrix.js
+++ src/mvpMatrix.js
@@ -1,11 +1,26 @@
 import matIV from "./minMatrix";
 var m = new matIV();
-export default (width, height) => {
-  // 各種行列の生成と初期化
-  var mMatrix = m.identity(m.create());
-  var vMatrix = m.identity(m.create());
-  var pMatrix = m.identity(m.create());
-  var mvpMatrix = m.identity(m.create());
+
+export function multiply(mat0, mat1) {
+  let ret = m.create();
+  m.multiply(mat0, mat1, ret);
+  return ret;
+}
+export function translate(mat, vec) {
+  let ret = m.create();
+  m.translate(mat, vec, ret);
+  return ret;
+}
+
+export function getModel(pos) {
+  let mat = m.identity(m.create());
+  return translate(mat, pos);
+}
+
+export function getPV({ width, height }) {
+  let vMatrix = m.identity(m.create());
+  let pMatrix = m.identity(m.create());
+  let ret = m.identity(m.create());
 
   // ビュー座標変換行列
   m.lookAt([0.0, 1.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
@@ -13,9 +28,5 @@ export default (width, height) => {
   // プロジェクション座標変換行列
   m.perspective(90, width / height, 0.1, 100, pMatrix);
 
-  // 各行列を掛け合わせ座標変換行列を完成させる
-  m.multiply(pMatrix, vMatrix, mvpMatrix);
-  m.multiply(mvpMatrix, mMatrix, mvpMatrix);
-
-  return mvpMatrix;
-};
+  return multiply(pMatrix, vMatrix);
+}
