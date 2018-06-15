27a1219896faf5d4d037d04c4a764258bfe56dfc
Koichi Miyamoto <m5813k@gmail.com>
2018-06-15 23:48:00 +0900

    Simulation をリファクタリング

--- src/misaki/Simulation.js
+++ src/misaki/Simulation.js
@@ -1,10 +1,36 @@
-import simDef from "../shader/simulation-def.frag";
-import simPos from "../shader/simulation-pos.frag";
-import simVel from "../shader/simulation-vel.frag";
+import shaderDef from "../shader/simulation-def.frag";
+import shaderPos from "../shader/simulation-pos.frag";
+import shaderVel from "../shader/simulation-vel.frag";
+
+const initArrayData = (dataPos, dataVel, dataDef) => {
+  const posArray = dataPos.image.data;
+  const velArray = dataVel.image.data;
+  const defArray = dataDef.image.data;
+
+  for (let i = 0; i < posArray.length; i += 4) {
+    const phi = Math.random() * 2 * Math.PI;
+    const theta = Math.random() * Math.PI;
+    const r = 0.8 + Math.random() * 2;
+
+    defArray[i + 0] = posArray[i + 0] = r * Math.sin(theta) * Math.cos(phi);
+    defArray[i + 1] = posArray[i + 1] = r * Math.sin(theta) * Math.sin(phi);
+    defArray[i + 2] = posArray[i + 2] = r * Math.cos(theta);
+
+    velArray[i + 3] = Math.random() * 1; // frames life
+  }
+};
+const setVelUniforms = velUniforms => {
+  velUniforms.timer = { value: 0.0 };
+  velUniforms.delta = { value: 0.0 };
+  velUniforms.speed = { value: 0.5 };
+  velUniforms.factor = { value: 0.5 };
+  velUniforms.evolution = { value: 0.5 };
+  velUniforms.radius = { value: 2.0 };
+};
+
 export default class Simulation {
   constructor(renderer, size) {
-    this.renderer = renderer;
-    this.size = size;
+    Object.assign(this, { renderer, size });
     this.init();
   }
 
@@ -15,58 +41,26 @@ export default class Simulation {
       this.renderer
     );
 
-    this.dataPos = this.gpuCompute.createTexture();
-    this.dataVel = this.gpuCompute.createTexture();
-    this.dataDef = this.gpuCompute.createTexture();
+    const dataPos = this.gpuCompute.createTexture();
+    const dataVel = this.gpuCompute.createTexture();
+    const dataDef = this.gpuCompute.createTexture();
 
-    var posArray = this.dataPos.image.data;
-    var velArray = this.dataVel.image.data;
-    var defArray = this.dataDef.image.data;
+    initArrayData(dataPos, dataVel, dataDef);
 
-    for (var i = 0, il = posArray.length; i < il; i += 4) {
-      var phi = Math.random() * 2 * Math.PI;
-      var theta = Math.random() * Math.PI;
-      var r = 0.8 + Math.random() * 2;
+    this.def = this.gpuCompute.addVariable("defTex", shaderDef, dataDef);
+    this.vel = this.gpuCompute.addVariable("velTex", shaderVel, dataVel);
+    this.pos = this.gpuCompute.addVariable("posTex", shaderPos, dataPos);
 
-      defArray[i + 0] = posArray[i + 0] = r * Math.sin(theta) * Math.cos(phi);
-      defArray[i + 1] = posArray[i + 1] = r * Math.sin(theta) * Math.sin(phi);
-      defArray[i + 2] = posArray[i + 2] = r * Math.cos(theta);
+    let dependencies = [this.pos, this.vel, this.def];
+    this.gpuCompute.setVariableDependencies(this.def, dependencies);
+    this.gpuCompute.setVariableDependencies(this.vel, dependencies);
+    this.gpuCompute.setVariableDependencies(this.pos, dependencies);
 
-      velArray[i + 3] = Math.random() * 100; // frames life
-      // if(i < 50) console.log(velArray[ i + 3 ])
-    }
+    let velUniforms = this.vel.material.uniforms;
+    setVelUniforms(velUniforms);
+    this.velUniforms = velUniforms;
 
-    this.def = this.gpuCompute.addVariable("defTex", simDef, this.dataDef);
-    this.vel = this.gpuCompute.addVariable("velTex", simVel, this.dataVel);
-    this.pos = this.gpuCompute.addVariable("posTex", simPos, this.dataPos);
-
-    this.gpuCompute.setVariableDependencies(this.def, [
-      this.pos,
-      this.vel,
-      this.def
-    ]);
-    this.gpuCompute.setVariableDependencies(this.vel, [
-      this.pos,
-      this.vel,
-      this.def
-    ]);
-    this.gpuCompute.setVariableDependencies(this.pos, [
-      this.pos,
-      this.vel,
-      this.def
-    ]);
-
-    // var posUniforms = this.pos.material.uniforms;
-    this.velUniforms = this.vel.material.uniforms;
-
-    this.velUniforms.timer = { value: 0.0 };
-    this.velUniforms.delta = { value: 0.0 };
-    this.velUniforms.speed = { value: 0.5 };
-    this.velUniforms.factor = { value: 0.5 };
-    this.velUniforms.evolution = { value: 0.5 };
-    this.velUniforms.radius = { value: 2.0 };
-
-    var error = this.gpuCompute.init();
+    const error = this.gpuCompute.init();
     if (error !== null) {
       console.error(error);
     }
