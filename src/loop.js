6dec4170a81250a97a132e04ed8e3221aed5d6bd
Koichi Miyamoto <m5813k@gmail.com>
2018-12-14 12:00:52 +0900

    let -> const as possible

--- src/loop.js
+++ src/loop.js
@@ -8,14 +8,14 @@ import {
   getPV
 } from "./mvpMatrix";
 
-let initCanvas = gl => {
+const initCanvas = gl => {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clearDepth(1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 };
 
-let makeUniformFunc = (prg, name, num) => pos => {
-  let loc = gl.getUniformLocation(prg, name);
+const makeUniformFunc = (prg, name, num) => pos => {
+  const loc = gl.getUniformLocation(prg, name);
   switch (num) {
     case 2:
       return gl.uniform2fv(loc, pos);
@@ -25,39 +25,39 @@ let makeUniformFunc = (prg, name, num) => pos => {
       return gl.uniform4fv(loc, pos);
   }
 };
-let makeUniformMatFunc = (prg, name) => mat => {
-  let loc = gl.getUniformLocation(prg, name);
+const makeUniformMatFunc = (prg, name) => mat => {
+  const loc = gl.getUniformLocation(prg, name);
   return gl.uniformMatrix4fv(loc, false, mat);
 };
 
 export default (gl, prg, size, indexLength) => {
-  let pv = getPV(size);
-  let setMat = makeUniformMatFunc(prg, "mvpMatrix");
-  let setModel = makeUniformMatFunc(prg, "mMatrix");
-  let setAmbient = makeUniformFunc(prg, "ambientColor", 4);
-  let drawWithMat = mat => {
+  const pv = getPV(size);
+  const setMat = makeUniformMatFunc(prg, "mvpMatrix");
+  const setModel = makeUniformMatFunc(prg, "mMatrix");
+  const setAmbient = makeUniformFunc(prg, "ambientColor", 4);
+  const drawWithMat = mat => {
     setMat(mat);
     // gl.drawArrays(gl.TRIANGLES, 0, 3);
     // インデックスを用いた描画命令
     gl.drawElements(gl.TRIANGLES, indexLength, gl.UNSIGNED_SHORT, 0);
   };
-  let setLight = makeUniformFunc(prg, "lightPosition", 3);
-  let setEye = makeUniformFunc(prg, "eyeDirection", 3);
+  const setLight = makeUniformFunc(prg, "lightPosition", 3);
+  const setEye = makeUniformFunc(prg, "eyeDirection", 3);
 
   let count = 0;
 
-  let loop = () => {
+  const loop = () => {
     initCanvas(gl);
 
     setEye([0, 10, 20]);
-    let t = count / 40;
-    let pos = [3 * Math.cos(t), 3 * Math.sin(t), 0];
+    const t = count / 40;
+    const pos = [3 * Math.cos(t), 3 * Math.sin(t), 0];
 
     setLight([10 * Math.cos(t), 10 * Math.sin(t), 0]);
     setAmbient([0.1, 0.1, 0.1, 1]);
 
     //prettier-ignore
-    let vecs = [
+    const vecs = [
       [0, 0, 0], 
       pos
     ];
