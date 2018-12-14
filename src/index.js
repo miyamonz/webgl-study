759a11cf4e73b0b27a7919169f914e1e45e5ecad
Koichi Miyamoto <m5813k@gmail.com>
2018-12-14 11:58:32 +0900

    const & some fix

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import startLoop from "./loop";
 import torus from "./torus";
 import sphere from "./sphere";
 let prependVBO = prg => {
-  // let [positions, normals, colors, index] = torus(4, 4, 1, 3);
-  let [positions, normals, colors, index] = sphere(64, 64, 2.0);
+  // const [positions, normals, colors, index] = torus(4, 4, 1, 3);
+  const [positions, normals, colors, index] = sphere(64, 64, 2.0);
 
   registerVBO(prg, positions, 3, "position");
   registerVBO(prg, normals, 3, "normal");
   registerVBO(prg, colors, 4, "color");
 
-  let ibo = create_ibo(index);
+  const ibo = create_ibo(index);
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
 
   return index.length;
@@ -22,24 +22,21 @@ let prependVBO = prg => {
 
 window.onload = function() {
   // canvasエレメントを取得
-  var c = document.getElementById("canvas");
-  c.width = 500;
-  c.height = 500;
-  let size = {
-    width: c.width,
-    height: c.height
-  };
+  const canvas = document.getElementById("canvas");
+  const width = 500;
+  const height = 500;
+  Object.assign(canvas, { width, height });
 
-  let gl = c.getContext("webgl");
+  const gl = canvas.getContext("webgl");
   window.gl = gl;
 
-  let { vert: v_shader, frag: f_shader } = create_shader({ frag, vert });
-  var prg = create_program(v_shader, f_shader);
+  const { vert: v_shader, frag: f_shader } = create_shader({ frag, vert });
+  const prg = create_program(v_shader, f_shader);
 
-  let len = prependVBO(prg);
+  const len = prependVBO(prg);
   gl.enable(gl.DEPTH_TEST);
   gl.depthFunc(gl.LEQUAL);
   gl.enable(gl.CULL_FACE);
 
-  startLoop(gl, prg, size, len);
+  startLoop(gl, prg, { width, height }, len);
 };
